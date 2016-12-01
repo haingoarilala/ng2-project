65e5e9797429ef113ceed074426d1b00bad4b1ea
haingoarilala <randrianjatovomonjyhaingoarilala@yahoo.fr>
2016-12-01 14:49:14 +0300

    new gh-pages version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{330:function(n,o){},603:function(n,o,c){n.exports=c(330)}},[603]);
+webpackJsonp([1,2],{369:function(n,o){},656:function(n,o,c){n.exports=c(369)}},[656]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
